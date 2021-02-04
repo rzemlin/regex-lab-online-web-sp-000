fd9e8d86e1c547c2eba052968a66c2e3a6fc624b
Ryan Zemlin <rzemlin@gmail.com>
2021-02-04 14:20:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,12 +9,7 @@ end
 def words_starting_with_un_and_ending_with_ing(text)
   counter = 0
   if text.scan(/\bun\w*ing\b/)
-    return true
-    counter += 1
-  else
-    return false
   end
-end
 
 def words_five_letters_long(text)
    text.match(/\d{5}/)
