b9a03a2ea805f975c34e0773a47e1f4cb29dd53e
Ryan Zemlin <rzemlin@gmail.com>
2021-02-04 14:13:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,25 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiouAEIOU]/)
+      return true
+    else
+      return false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/^un\w*ing$/)
+    return true
+  else
+    return false
+  end
 end
 
 def words_five_letters_long(text)
+   if text.match(/\d{5}/)
+      return true
+    else
+      return false
+    end
 
 end
 
